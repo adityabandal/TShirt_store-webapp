d549c786a70ad17b58f7d574224ebdc04c1c3ed5
adityabandal <adityabandal007@gmail.com>
2020-09-20 19:14:12 +0530

    signin controller

--- projbackend/controllers/auth.js
+++ projbackend/controllers/auth.js
@@ -1,6 +1,7 @@
 var User = require("../models/user");
-const { use } = require("../routes/auth");
 const { check, validationResult } = require("express-validator");
+var jwt = require("jsonwebtoken");
+var expressJwt = require("express-jwt");
 
 exports.signup = (req, res) => {
     const errors = validationResult(req);
@@ -23,6 +24,40 @@ exports.signup = (req, res) => {
             name: user.name,
             email: user.email,
             id: user._id,
+        });
+    });
+};
+
+exports.signin = (req, res) => {
+    const { email, password } = req.body;
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        return res.status(422).json({
+            error: errors.array()[0].msg,
+        });
+    }
+
+    User.findOne({ email }, (err, user) => {
+        if (err || !user) {
+            return res.status(400).json({
+                error: "User email does not exist",
+            });
+        }
+        if (!user.authenticate(password)) {
+            return res.status(401).json({
+                error: "Email and password do  not match",
+            });
+        }
+        // Create token
+        const token = jwt.sign({ _id: user._id }, process.env.SECRET);
+        // put token in cookie
+        res.cookie("token", token, { expire: new Date() + 9999 });
+        // send response to frontend
+        const { _id, name, email, role } = user;
+        res.json({
+            token,
+            user: { _id, name, email, role },
         });
     });
 };
--- projbackend/routes/auth.js
+++ projbackend/routes/auth.js
@@ -1,9 +1,13 @@
 var express = require("express");
 var router = express.Router();
 
-const { signout, signup } = require("../controllers/auth");
+const { signout, signup, signin } = require("../controllers/auth");
 const { check, validationResult } = require("express-validator");
-
+router.get("/hi", (req, res) => {
+    res.json({
+        msg: "hello there",
+    });
+});
 router.post(
     "/signup",
     [
@@ -17,6 +21,18 @@ router.post(
     ],
     signup
 );
+
+router.post(
+    "/signin",
+    [
+        check("email", "Email is required").isEmail(),
+        check("password", "Password field is required").isLength({
+            min: 1,
+        }),
+    ],
+    signin
+);
+
 router.get("/signout", signout);
 
 module.exports = router;
